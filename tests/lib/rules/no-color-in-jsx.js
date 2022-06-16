5242c75df89c99c247d05ccf7793fc0181db3022
nmsn <136696700@qq.com>
2022-06-17 00:24:44 +0800

    feat(rules): no-color-in-jsx init

---
+++ tests/lib/rules/no-color-in-jsx.js
@@ -0,0 +1,23 @@
+const { RuleTester } = require("eslint");
+
+const rule = require("../../../lib/rules/no-color-in-jsx");
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, ecmaFeatures: { jsx: true } },
+});
+
+// const errMsg = (warnWord) => `The code here needs to be processed, ${warnWord}`;
+
+ruleTester.run("no-color-in-jsx", rule, {
+  valid: ["<div style={{ color: 123 }}>123</div>"],
+  invalid: [
+    {
+      code: "<div style={{ color: 'red' }}>123</div>",
+      errors: [
+        {
+          message: "参数最多不能超过3个",
+        },
+      ],
+    },
+  ],
+});
