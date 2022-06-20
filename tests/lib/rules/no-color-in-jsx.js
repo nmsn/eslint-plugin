427b432f4f7486f4a313623ffd4c5f91397a54a4
nmsn <136696700@qq.com>
2022-06-21 00:13:11 +0800

    chore(rules): 完善 no-color-in-jsx 逻辑

--- tests/lib/rules/no-color-in-jsx.js
+++ tests/lib/rules/no-color-in-jsx.js
@@ -9,13 +9,37 @@ const ruleTester = new RuleTester({
 // const errMsg = (warnWord) => `The code here needs to be processed, ${warnWord}`;
 
 ruleTester.run("no-color-in-jsx", rule, {
-  valid: ["<div style={{ color: 123 }}>123</div>"],
+  valid: ["<div style={{ color: 123 }}>123</div>", "<Icon type={1}>123</Icon>"],
   invalid: [
     {
       code: "<div style={{ color: 'red' }}>123</div>",
       errors: [
         {
           message: "Don't use color string 'red' in jsx",
+        },
+      ],
+    },
+    {
+      code: "<div style={{ color: '#000' }}>#000</div>",
+      errors: [
+        {
+          message: "Don't use color string '#000' in jsx",
+        },
+      ],
+    },
+    {
+      code: "<Icon color='red'>123</Icon>",
+      errors: [
+        {
+          message: "Don't use color string 'red' in jsx",
+        },
+      ],
+    },
+    {
+      code: "<Icon color='#000'>#000</Icon>",
+      errors: [
+        {
+          message: "Don't use color string '#000' in jsx",
         },
       ],
     },
