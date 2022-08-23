1f71ae35bcc8af8ba519e2cbe72b21bc2b263c49
nmsn <136696700@qq.com>
2022-08-23 16:47:56 +0800

    feat(rules): add a rule no-color-in-jex-attribute

--- tests/lib/rules/no-color-in-jsx-attribute.js
+++ tests/lib/rules/no-color-in-jsx-attribute.js
@@ -1,6 +1,6 @@
 const { RuleTester } = require("eslint");
 
-const rule = require("../../../lib/rules/no-color-in-jsx");
+const rule = require("../../../lib/rules/no-color-in-jsx-attribute");
 
 const ruleTester = new RuleTester({
   parserOptions: { ecmaVersion: 6, ecmaFeatures: { jsx: true } },
@@ -8,14 +8,14 @@ const ruleTester = new RuleTester({
 
 // const errMsg = (warnWord) => `The code here needs to be processed, ${warnWord}`;
 
-ruleTester.run("no-color-in-jsx", rule, {
+ruleTester.run("no-color-in-jsx-attribute", rule, {
   valid: ["<div style={{ color: 123 }}>123</div>", "<Icon type={1}>123</Icon>"],
   invalid: [
     {
       code: "<div style={{ color: 'red' }}>123</div>",
       errors: [
         {
-          message: "Don't use color string 'red' in jsx",
+          message: "Don't use color string 'red' in jsx attribute",
         },
       ],
     },
@@ -23,7 +23,7 @@ ruleTester.run("no-color-in-jsx", rule, {
       code: "<div style={{ color: '#000' }}>#000</div>",
       errors: [
         {
-          message: "Don't use color string '#000' in jsx",
+          message: "Don't use color string '#000' in jsx attribute",
         },
       ],
     },
@@ -31,7 +31,7 @@ ruleTester.run("no-color-in-jsx", rule, {
       code: "<Icon color='red'>123</Icon>",
       errors: [
         {
-          message: "Don't use color string 'red' in jsx",
+          message: "Don't use color string 'red' in jsx attribute",
         },
       ],
     },
@@ -39,7 +39,7 @@ ruleTester.run("no-color-in-jsx", rule, {
       code: "<Icon color='#000'>#000</Icon>",
       errors: [
         {
-          message: "Don't use color string '#000' in jsx",
+          message: "Don't use color string '#000' in jsx attribute",
         },
       ],
     },
